b2ce73891af64ad1c3b39077b0e11638104076bc
CML <chrismanuel.lorando@gmail.com>
2025-11-11 03:37:47 +0700

    feat(service): add AiServiceInterface adjustments

--- app/Services/AiServiceInterface.php
+++ app/Services/AiServiceInterface.php
@@ -7,19 +7,21 @@ interface AiServiceInterface
     /**
      * Send a message with streaming response
      *
-     * @param string $conversationId
-     * @param string $content
-     * @param callable $onChunk Callback function that receives each chunk
-     * @param string|null $model Model to use for the request
+     * @param  callable  $onChunk  Callback function that receives each chunk
+     * @param  string|null  $model  Model to use for the request
      * @return mixed
      */
     public function sendMessageWithStream(string $conversationId, string $content, callable $onChunk, ?string $model = null);
 
     /**
      * Cancel a model response with the given ID
-     *
-     * @param string $responseId
-     * @return array
      */
     public function cancelResponse(string $responseId): array;
+
+    /**
+     * Send a file to be checked
+     *
+     * @return mixed
+     */
+    public function createDocumentResponse(string $document, ?string $instruction = null, ?string $model = null);
 }
